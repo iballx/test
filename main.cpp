b55249dd5b7044091eb82f43bd07ec39dd2ac4b1
Andrew Todd <andrew@todda.uk>
2016-11-23 22:00:03 +0000

    Added cpp files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello world!" << endl;
+    vector<string> clArgs {};
+    for (int i {}; i != argc; ++i)
+    {
+        clArgs.push(argv[i]);
+    }
+    
+    for (vector<string>.iterator i {clArgs.begin()}; i != clArgs.end(); ++i)
+    {
+        cout << i << endl;
+    }
+    cout << argc << endl;
+    return 0;
+}
